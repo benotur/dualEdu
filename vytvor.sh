c0ada6cb9ce312026bb7d17cbfd75803ee7dc83c
benotur <ben.turcani@gmail.com>
2025-09-25 09:26:16 +0200

    vytvor.sh updates

--- vytvor.sh
+++ vytvor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir 2025/{januar/{ZIL,SISL},februar/{ZIL,SISL},marec/{ZIL,SISL},april/{ZIL,SISL},maj/{ZIL,SISL},jun/{ZIL,SISL},jul/{ZIL,SISL},august/{ZIL,SISL},september/{ZIL,SISL},oktober/{ZIL,SISL},november/{ZIL,SISL},december/{ZIL,SISL}}
+months=(januar februar marec april maj jun jul august september oktober november december)
+subs=(ZIL SISL)
 
-for month in januar februar marec april maj jun jul august september oktober november december; do
-for sub in ZIL SISL; do
-echo "Moje Poznamky" > 2025/$month/$sub/poznamky.txt
-done
+for month in "${months[@]}"; do
+    for sub in "${subs[@]}"; do
+        mkdir -p "2025/$month/$sub"
+        echo "Moje Poznamky" > "2025/$month/$sub/poznamky.txt"
+    done
 done
